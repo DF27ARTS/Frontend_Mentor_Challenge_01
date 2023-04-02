a79c0a097ce68bb8978afdb5029ba7abb2485533
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2023-04-02 13:47:54 -0500

    First commit

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const summaryBoldNumbers = document.querySelectorAll(".summary-bold-number");
+const summaryOptionTexts = document.querySelectorAll(".summary-option-text");
+const summaryOptionIcons = document.querySelectorAll(".summary-option-icon");
+
+(async () => {
+  const response = await fetch("../data.json");
+  const data = await response.json();
+
+  summaryBoldNumbers.forEach((item, index) => {
+    item.innerText = data[index].score;
+    summaryOptionTexts[index].innerText = data[index].category;
+    summaryOptionIcons[index].src = data[index].icon;
+  });
+})();
